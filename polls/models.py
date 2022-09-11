fc01b0b892d6b3ee67104b3c639309658ab61163
panitnt <panittatanya@gmail.com>
2022-09-11 11:47:31 +0700

    add two methods

--- polls/models.py
+++ polls/models.py
@@ -16,7 +16,7 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-        
+
     def is_published(self):
         return self.pub_date <= timezone.now()
 
