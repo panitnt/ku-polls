ee15ea35b9cdb8d56f6b5f05be9b023eda441a70
panitnt <panittatanya@gmail.com>
2022-09-11 19:51:29 +0700

    redirect

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 from django.utils import timezone
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.contrib import messages
 
 from polls.models import Choice, Question
 
@@ -38,9 +39,11 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
     def get_queryset(self):
         """Excludes any questions that aren't published yet."""
         return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -58,3 +61,7 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+def redirect(self):
+    return HttpResponseRedirect(reverse('polls:index'))
